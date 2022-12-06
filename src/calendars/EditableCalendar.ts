e279042f3edf3e8d48880a990d31e3ffe7e7bfd7
Davis Haupt <davis.haupt@gmail.com>
2022-12-06 08:42:47 -0500

    TODOs for modifying the filesystem

--- src/calendars/EditableCalendar.ts
+++ src/calendars/EditableCalendar.ts
@@ -9,4 +9,9 @@ export abstract class EditableCalendar extends Calendar {
 		fileCache: CachedMetadata,
 		contents: string
 	): OFCEvent[];
+
+	// TODO: Given an old and new event, return a pair of filenames will need to be modified to
+	// modify the event on disk. One can potentially be null if the event isn't moving between files.
+
+	// TODO: Given the text of the oldFile/newFile, return NEW text for the old/new file.
 }
