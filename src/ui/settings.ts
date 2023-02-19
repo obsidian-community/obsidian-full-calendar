765940f1bf5611acb14135ddee8966258e18427b
Sergei <igelbox@gmail.com>
2023-02-19 23:41:32 +0400

    fix mobile initial view saving

--- src/ui/settings.ts
+++ src/ui/settings.ts
@@ -205,6 +205,7 @@ export class FullCalendarSettingTab extends PluginSettingTab {
                 dropdown.setValue(this.plugin.settings.initialView.mobile);
                 dropdown.onChange(async (initialView) => {
                     this.plugin.settings.initialView.mobile = initialView;
+                    await this.plugin.saveSettings();
                 });
             });
 
